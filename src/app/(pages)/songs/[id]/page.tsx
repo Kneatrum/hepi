ea63cb6f935e772b9cabfaa831cf42326f157f01
Kneatrum <martin.mwiti.n@gmail.com>
2025-07-04 01:54:58 +0300

    code cleanup

--- src/app/(pages)/songs/[id]/page.tsx
+++ src/app/(pages)/songs/[id]/page.tsx
@@ -192,7 +192,7 @@ export default function Page() {
                 </Card>
               </Grid>
             </Grid>
-            <MediaPlayer song={song} />
+            {/* <MediaPlayer song={song} /> */}
           </>
         )}
         
